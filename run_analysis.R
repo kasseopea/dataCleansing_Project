72047f6d0f69ed0030e85c086e1a9eda2341e484
kasseopea <kasseopea@gmail.com>
2015-05-25 01:09:42 +0800

    Update runanalysis

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,7 @@
 #Course Project : Mariah Azlan
-
 library(plyr)
+library(dplyr)
+
 
 #1- Merges the training and the test sets to create one data set.
   #READ ALL FILES
@@ -44,19 +45,22 @@ library(plyr)
   mean_cols <- grep ( "std\\(\\)", col_names) # extract the column numbers with std()
   valid_cols <- c(1,2,std_cols, mean_cols) # adding subject and activity as the first two columns in addition std and mean cols
   mean_std_data <- master_data[, valid_cols] 
+  write.table(average_subject_id, file="Project_q2.txt", row.name=FALSE)
 
 #3 - Uses descriptive activity names to name the activities in the data set
   master_activity <- data.frame(master_data[,2]) #extract activity IDs in the second column
-  names(master_activity)[1] <- "id"
-  temp_str <- join(master_activity, activity_labels)
-  master_data[,2] <- temp_str[,2]
+  names(master_activity)[1] <- "id" 
+  temp_str <- join(master_activity, activity_labels) #match the id with the descriptive names in a temporary dataframe
+  master_data[,2] <- temp_str[,2] #restore the descriptive activity names back in second column of full data (master)
   
-# #4 Appropriately labels the data set with descriptive variable names
-#   #variable subject had already been added earlier, now just rename
-#   colnames(master_data)[1] <- "Subject"
-#   colnames(master_data)[2] <- "Activity"
-#   
-# #5 From the data set in step 4, creates a second, independent tidy data set 
-# #  with the average of each variable for each activity and each subject.
-# 
-#   
+#4 Appropriately labels the data set with descriptive variable names
+  #variable subject had already been added earlier before merging using colnames, now just rename the first two columns
+  colnames(master_data)[1] <- "Subject"
+  colnames(master_data)[2] <- "Activity"
+  
+#5 From the data set in step 4, creates a second, independent tidy data set 
+#  with the average of each variable for each activity and each subject.
+tempcolnames <- names(master_data[3:ncol(master_data)]) #extract the col names that need to be averaged
+temp_melt <- melt(master_data, id= c("Subject", "Activity"), measure.vars = tempcolnames) # make the data long by Subject and Activity
+average_subject_id <- dcast( temp_melt, Subject + Activity ~ variable, mean) # now make it wide again
+write.table(average_subject_id, file="Project_q5.txt", row.name=FALSE)
